81de1a8c8e0009dd4ccab49159e6628721195ce3
Tony Malagisi <mtb3000gt@gmail.com>
2015-01-30 16:50:30 -0500

    Remove unused stuff and switch kernel and tool chain
    Change-Id: I90c8b372263b6c4277fee4b1120e75031e93b9b8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,8 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/d2-common/include
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
-TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
+TARGET_KERNEL_CONFIG        := bms_d2_defconfig
+TARGET_GCC_VERSION_ARM := 4.9-linaro
 
 # LIQUID OPTIMIZATIONS
 LIQUIFY := true
